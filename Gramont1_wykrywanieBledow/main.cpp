f9a5a95fda1c058ec1c29862f939eb4b8ea63803
Mateusz Bielski <mateo.bass@interia.pl>
2022-01-03 12:46:59 +0100

    tylko main.cpp po wprowadzeniu funkcji fabrykujacej w donarysowania

--- Gramont1_wykrywanieBledow/main.cpp
+++ Gramont1_wykrywanieBledow/main.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "../src/Shared/donarysowania.h"
+#include "../src/Shared/prostytrojkat.h"
 
 int main(int argc, char **argv)
 {
 	
     printf("sizeof(DoNarysowania) %d\n",sizeof(DoNarysowania));
-    auto rys(make_shared<DoNarysowania>());
-    printf("auto rys(make_shared<DoNarysowania>()) sizeof *rys %d\n",sizeof *rys);
+    auto rys(DoNarysowania::Create());
+    auto pRys = rys->WskaznikNaMnie();
+    printf("auto rys(DoNarysowania::Create()) sizeof *rys %d\n",sizeof *rys);
+    printf("rys.use_count() %d\n",rys.use_count());
+    printf("pRys.use_count() %d\n",pRys.use_count());
+    
+    auto trojkat(ProstyTrojkat::Create());
+    printf("ile vertexow %d\n",trojkat->ileVertexow);
+    
+    /*
     shared_ptr<Transformowalne> trans(make_shared<DoNarysowania>());
     printf("shared_ptr<Transformowalne> trans(make_shared<DoNarysowania>()) sizeof *trans %d\n",sizeof *trans);
     shared_ptr<Transformowalne> trans2 = make_shared<DoNarysowania>();
@@ -14,5 +23,6 @@ int main(int argc, char **argv)
     shared_ptr<Transformowalne> trans3 = rys;
     printf("shared_ptr<Transformowalne> trans3 = rys sizeof *trans3 %d\n",sizeof *trans3);
     printf("sizeof(Transformowalne) %d\n",sizeof(Transformowalne));
+     * */
 	return 0;
 }
